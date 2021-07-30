e5c8181ac551b0cdd2eb715898df3381ee21002d
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2021-07-30 19:43:20 +0200

    transport-service project init-commit.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

---
+++ transport-service/src/main/java/org/epal/transportservice/TransportServiceApplication.java
@@ -0,0 +1,13 @@
+package org.epal.transportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportServiceApplication.class, args);
+    }
+
+}
