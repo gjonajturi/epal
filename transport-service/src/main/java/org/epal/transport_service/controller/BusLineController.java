bf54118d932f81d9677100c1a47d1e54338ddb16
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2021-08-08 11:38:49 +0200

    -New notification-service project & supplier configs.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

--- transport-service/src/main/java/org/epal/transport_service/controller/BusLineController.java
+++ transport-service/src/main/java/org/epal/transport_service/controller/BusLineController.java
@@ -1,9 +1,11 @@
 package org.epal.transport_service.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.epal.commons.transport.model.BusLine;
 import org.epal.transport_service.service.BusLineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,10 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/bus_line")
+@Slf4j
 public class BusLineController {
 
     @Autowired
     private BusLineService busLineService;
+
+    @Autowired
+    private StreamBridge streamBridge;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -27,6 +33,10 @@ public class BusLineController {
     public BusLine findById(@PathVariable Long busLineId) {
         BusLine busLine = busLineService.findById(busLineId)
                 .orElseThrow(() -> new RuntimeException("BusLine with id " + busLineId + " not found."));
+
+        log.info("Sending Bus line "+busLineId+" details to notification-service.");
+        streamBridge.send("notificationEventSupplier-out-0",busLine.getId());
+
         return busLine;
     }
 
